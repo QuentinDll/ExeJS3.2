2b9421cbdd2ce6c519677fc6a7307b6cd027b98c
QuentinDll <quentin.delplace13@gmail.com>
2018-06-25 12:24:59 +0200

    creation function on  anchor shows text, display text

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//function shows
+function toggleOn(){
+    document.getElementById('text').style.visibility = 'visible';
+}
+//function mask
+function toggleOff(){
+    document.getElementById('text').style.visibility = 'hidden';
+}
